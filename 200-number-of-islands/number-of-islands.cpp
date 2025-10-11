7c705a860352078e234c0cd8a46aa7546a3fbc74
Madhurya kosti <88713046+madhurya1320@users.noreply.github.com>
2025-10-11 13:14:58 -0500

    Time: 22 ms (88.29%) | Memory: 16.3 MB (69.89%) - LeetSync

--- 200-number-of-islands/number-of-islands.cpp
+++ 200-number-of-islands/number-of-islands.cpp
@@ -1,12 +1,9 @@
 class Solution {
-    vector<vector<int>>directions = {{1,0}, {-1, 0} ,{0, 1} ,{0, -1}};
 public:
-    //vector<vector<int>>directions = {{1,0}, {-1, 0} ,{0, 1} ,{0, -1}};
     int numIslands(vector<vector<char>>& grid) {
-        int row = grid.size(); int col = grid[0].size();
-        int count = 0;
-        for(int i = 0; i<row; i++) {
-            for(int j = 0; j<col; j++) {
+        int r = grid.size(); int c = grid[0].size(); int count = 0;
+        for(int i = 0; i<r; i++) {
+            for(int j = 0; j<c; j++) {
                 if(grid[i][j] == '1') {
                     dfs(grid, i, j);
                     count++;
@@ -15,14 +12,17 @@ public:
         }
         return count;
     }
-    void dfs(vector<vector<char>>& grid, int r, int c) {
-        if(r<0 || c<0 || r >= grid.size() || c >= grid[0].size() || grid[r][c] == '0') {
+    vector<vector<int>>directions = {{1, 0}, {-1, 0}, {0,1}, {0,-1}};
+    void dfs(vector<vector<char>>& grid, int ro, int cl) {
+        if(ro < 0 || cl < 0 || ro >= grid.size() || cl >=grid[0].size() || grid[ro][cl] == '0' ) {
             return;
         }
-        grid[r][c] = '0';
-        for(int i = 0; i<directions.size(); i++) {
-            dfs(grid, r + directions[i][0], c + directions[i][1]);
-        }
+        grid[ro][cl] = '0';
+        for(int i = 0; i<4; i++) {
+            //int nr = ro + directions[i][0];
+            //int nc = cl + directions[i][1];
 
+            dfs(grid, ro + directions[i][0], cl + directions[i][1]);
+        }
     }
 };
