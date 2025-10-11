018bd6ad5ca248c3451007082066fbfdd55cfb89
Madhurya kosti <88713046+madhurya1320@users.noreply.github.com>
2025-10-11 16:19:21 -0500

    Time: 11 ms (15.25%) | Memory: 31.6 MB (20.02%) - LeetSync

--- 695-max-area-of-island/max-area-of-island.cpp
+++ 695-max-area-of-island/max-area-of-island.cpp
@@ -1,31 +1,37 @@
 class Solution {
 public:
     int maxAreaOfIsland(vector<vector<int>>& grid) {
-        int r = grid.size(); int c = grid[0].size(); int maxcount = 0; 
+        int r = grid.size(); int c = grid[0].size(); int maxres = 0;
         for(int i = 0; i<r; i++) {
             for(int j = 0; j<c; j++) {
                 if(grid[i][j] == 1) {
-                    int count = 0;
-                    dfs(grid, i, j, count);
-                    maxcount = max(maxcount,count);
+                  maxres = max(maxres, bfs(grid, i , j));
+
                 }
             }
         }
-        return maxcount;
+        return maxres;
     }
-    vector<vector<int>>directions = {{1,0}, {-1, 0}, {0,1}, {0,-1}};
+    vector<vector<int>>directions = {{1,0}, {-1,0}, {0,1}, {0,-1}};
+    int bfs(vector<vector<int>>& grid, int r, int c) {
+        queue<pair<int, int>>q;
+        grid[r][c] = 0;
+        q.push({r,c});
+        int res = 1;
+        while(!q.empty()) {
+            auto node = q.front(); q.pop();
+            for(int i = 0; i<4; i++) {
+            int nr = node.first + directions[i][0];
+            int nc = node.second + directions[i][1];
 
-    void dfs(vector<vector<int>>& grid, int row, int col, int& count) {
-        if(row < 0 || col <0 || row>=grid.size() || col >=grid[0].size() || grid[row][col] == 0) return;
+            if(nr>=0 && nc >= 0 && nr < grid.size() && nc < grid[0].size() && grid[nr][nc] == 1) {
+                res++;
+                q.push({nr, nc});
+                grid[nr][nc] = 0;
+            }
 
-        grid[row][col] = 0;
-        count = count + 1; 
-
-    for(int i = 0; i<4; i++) {
-        int nr = row + directions[i][0];
-        int nc = col + directions[i][1];
-            dfs(grid, nr, nc, count);
-        
-    }
+            }
+        }
+        return res;
     }
 };
