cccc67b62ce0b037526ec607a83b912cee6ff956
Madhurya kosti <88713046+madhurya1320@users.noreply.github.com>
2025-12-03 11:31:47 -0500

    Time: 0 ms (100.00%) | Memory: 27.6 MB (65.35%) - LeetSync

--- 695-max-area-of-island/max-area-of-island.cpp
+++ 695-max-area-of-island/max-area-of-island.cpp
@@ -1,37 +1,35 @@
 class Solution {
+    int directions[4][2] = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
 public:
     int maxAreaOfIsland(vector<vector<int>>& grid) {
-        int r = grid.size(); int c = grid[0].size(); int maxres = 0;
-        for(int i = 0; i<r; i++) {
-            for(int j = 0; j<c; j++) {
-                if(grid[i][j] == 1) {
-                  maxres = max(maxres, bfs(grid, i , j));
+        int ROWS = grid.size(), COLS = grid[0].size();
+        int Max_area = 0;
+        int count = 0;
 
+        for (int r = 0; r < ROWS; r++) {
+            for (int c = 0; c < COLS; c++) {
+                if (grid[r][c] == 1) {
+                    dfs(grid, r, c,count);
+                    Max_area = max(Max_area, count);
+                    count = 0;
                 }
             }
         }
-        return maxres;
+
+        return Max_area;
     }
-    vector<vector<int>>directions = {{1,0}, {-1,0}, {0,1}, {0,-1}};
-    int bfs(vector<vector<int>>& grid, int r, int c) {
-        queue<pair<int, int>>q;
-        grid[r][c] = 0;
-        q.push({r,c});
-        int res = 1;
-        while(!q.empty()) {
-            auto node = q.front(); q.pop();
-            for(int i = 0; i<4; i++) {
-            int nr = node.first + directions[i][0];
-            int nc = node.second + directions[i][1];
 
-            if(nr>=0 && nc >= 0 && nr < grid.size() && nc < grid[0].size() && grid[nr][nc] == 1) {
-                res++;
-                q.push({nr, nc});
-                grid[nr][nc] = 0;
-            }
-
-            }
+    void dfs(vector<vector<int>>& grid, int r, int c, int &count) {
+        if (r < 0 || c < 0 || r >= grid.size() ||
+            c >= grid[0].size() || grid[r][c] == 0) {
+            return ;
         }
-        return res;
-    }
+
+        grid[r][c] = 0;
+        count++;
+        for (int i = 0; i < 4; i++) {
+                 dfs(grid, r + directions[i][0],
+                             c + directions[i][1], count);
+        }
+    }
 };
