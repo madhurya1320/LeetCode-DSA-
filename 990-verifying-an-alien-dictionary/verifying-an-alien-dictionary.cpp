6a8b4be1068b83fad12c9843e5dfcf459dc29b89
Madhurya kosti <88713046+madhurya1320@users.noreply.github.com>
2026-02-14 16:15:20 -0800

    Time: 0 ms (100.00%) | Memory: 12.6 MB (46.53%) - LeetSync

---
+++ 990-verifying-an-alien-dictionary/verifying-an-alien-dictionary.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool isAlienSorted(vector<string>& words, string order) {
+        unordered_map<char, int>mp;
+        for(int i = 0; i<order.size(); i++) {
+            mp[order[i]] = i;
+        }
+        for(int i = 0; i<words.size() - 1; i++) {
+            string s1 = words[i];
+            string s2 = words[i+1];
+
+            for(int j = 0; j <s1.size(); j++) {
+                if(s2.size() == j) return false;
+                if(s1[j] != s2[j] ) {
+                    if(mp[s1[j]] > mp[s2[j]]) {
+                        return false;
+                    }
+                    break;
+                } 
+            }
+        }
+        return true;
+    }
+};
